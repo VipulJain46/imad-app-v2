aaba8b0255258c1db4e77486b782e19b3bb727e1
VipulJain46 <vipulnptel@gmail.com>
2017-02-10 20:24:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,14 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
 var img = document.getElementById('madi');
+var marginleft = 0;
+ function moveright()
+    {
+        marginleft=marginleft + 10;
+        img.style.marginleft=marginleft + 'px';
+    }
 img.onclick = function(){
+   
+    var interval = setInterval(moveright,100);
     img.style.marginleft = '100px';
 };
